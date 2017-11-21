f9058b82bc353e65bd70f2dd3c9d63d630b66206
Aleksey Ishutin <ishutin.alexey@gmail.com>
2017-11-21 16:13:17 +0300

    tests improvements

--- tests/Entity/AudioTest.php
+++ tests/Entity/AudioTest.php
@@ -40,13 +40,20 @@ final class AudioTest extends TestCase
     public function testProperties(): void
     {
         $audio = new Audio($this->fileId, $this->duration);
+
+        $this->assertEquals($this->fileId, $audio->id);
+        $this->assertEquals($this->duration, $audio->duration);
+        $this->assertNull($audio->performer);
+        $this->assertNull($audio->title);
+        $this->assertNull($audio->mimeType);
+        $this->assertNull($audio->fileSize);
+
+
         $audio->performer = $this->performer;
         $audio->title = $this->title;
         $audio->mimeType = $this->mimeType;
         $audio->fileSize = $this->fileSize;
 
-        $this->assertEquals($this->fileId, $audio->id);
-        $this->assertEquals($this->duration, $audio->duration);
         $this->assertEquals($this->performer, $audio->performer);
         $this->assertEquals($this->title, $audio->title);
         $this->assertEquals($this->mimeType, $audio->mimeType);
--- tests/Entity/MessageTest.php
+++ tests/Entity/MessageTest.php
@@ -3,8 +3,11 @@
 namespace Test\Entity;
 
 use PHPUnit\Framework\TestCase;
+use Telegram\Entity\Audio;
 use Telegram\Entity\Chat;
 use Telegram\Entity\Message;
+use Telegram\Entity\MessageEntity;
+use Telegram\Entity\User;
 
 final class MessageTest extends TestCase
 {
@@ -28,5 +31,42 @@ final class MessageTest extends TestCase
 
         $this->assertEquals($this->id, $message->id);
         $this->assertEquals($this->date, $message->date);
+        $this->assertInstanceOf(Chat::class, $message->chat);
+
+        $this->assertEmpty($message->entities);
+        $this->assertNull($message->audio);
+        $this->assertNull($message->from);
+        $this->assertNull($message->text);
+        $this->assertNull($message->replyTo);
+
+        $message->text = 'test';
+        $message->audio = new Audio('test', 1234);
+        $message->entities = [new MessageEntity('hashtag', 0, 5)];
+        $message->from = new User(1, 'test', 'test', 'test', 'en', false);
+        $message->replyTo = new Message(1, 2, new Chat(2, 'private'));
+
+        $this->assertEquals('test', $message->text);
+        $this->assertNotEmpty($message->entities);
+
+        $this->assertInstanceOf(Audio::class, $message->audio);
+        $this->assertInstanceOf(User::class, $message->from);
+        $this->assertInstanceOf(Message::class, $message->replyTo);
+    }
+
+    public function testGetEntitiesValues(): void
+    {
+        $message = new Message(
+            $this->id,
+            $this->date,
+            new Chat(1, 'private')
+        );
+
+        $message->text = '#test super text';
+        $message->entities = [new MessageEntity('hashtag', 0, 5)];
+
+        $this->assertContains(
+            '#test',
+            $message->getEntitiesValues('hashtag')
+        );
     }
 }
