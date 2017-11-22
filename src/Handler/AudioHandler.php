c9c72bdec9f05638ceb71d9e25fc89b5a37c8b12
Aleksey Ishutin <ishutin.alexey@gmail.com>
2017-11-22 18:20:06 +0300

    added AudioHandler

---
+++ src/Handler/AudioHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Telegram\Handler;
+
+use Telegram\Exception\HandlerException;
+use Telegram\Kernel\RequestInterface;
+use Telegram\Kernel\ResponseInterface;
+
+class AudioHandler implements HandlerInterface
+{
+    /**
+     * @var ActionInterface
+     */
+    private $action;
+
+    public function handle(RequestInterface $request, ResponseInterface $response): void
+    {
+        if (empty($this->action)) {
+            throw new HandlerException('Invalid handler action');
+        }
+
+        if ($request->getMessage()->audio) {
+            $this->action->handle($request, $response);
+        }
+    }
+
+    public function on(ActionInterface $action)
+    {
+        $this->action = $action;
+    }
+}
