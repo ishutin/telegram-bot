4486621914c546b2b5c722138b571829e5d00232
Aleksey Ishutin <ishutin.alexey@gmail.com>
2017-11-25 13:39:34 +0300

    all events handler

--- src/Handler/UpdateEventHandler.php
+++ src/Handler/UpdateEventHandler.php
@@ -7,6 +7,7 @@ use Telegram\Kernel\RequestInterface;
 
 class UpdateEventHandler extends AbstractHandler
 {
+    public const EVENT_ALL = 'all';
     public const EVENT_MESSAGE = 'message';
     public const EVENT_CHANNEL_POST = 'channel_post';
     public const EVENT_EDITED_MESSAGE = 'edited_message';
@@ -18,37 +19,41 @@ class UpdateEventHandler extends AbstractHandler
 
     public function handle(RequestInterface $request, Update $update): void
     {
-        if ($action = $this->getAction(self::EVENT_MESSAGE)) {
-            if ($update->getMessage()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_CHANNEL_POST)) {
-            if ($update->getChannelPost()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_EDITED_MESSAGE)) {
-            if ($update->getEditedMessage()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_EDITED_CHANNEL_POST)) {
-            if ($update->getEditedChannelPost()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_INLINE_QUERY)) {
-            if ($update->getInlineQuery()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_CHOSEN_INLINE_RESULT)) {
-            if ($update->getChosenInlineResult()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_CALLBACK_QUERY)) {
-            if ($update->getCallbackQuery()) {
-                $action->run($request, $update);
-            }
-        } elseif ($action = $this->getAction(self::EVENT_PRE_CHECKOUT_QUERY)) {
-            if ($update->getPreCheckoutQuery()) {
-                $action->run($request, $update);
+        if ($action = $this->getAction(self::EVENT_ALL)) {
+            $action->run($request, $update);
+        } else {
+            if ($action = $this->getAction(self::EVENT_MESSAGE)) {
+                if ($update->getMessage()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_CHANNEL_POST)) {
+                if ($update->getChannelPost()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_EDITED_MESSAGE)) {
+                if ($update->getEditedMessage()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_EDITED_CHANNEL_POST)) {
+                if ($update->getEditedChannelPost()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_INLINE_QUERY)) {
+                if ($update->getInlineQuery()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_CHOSEN_INLINE_RESULT)) {
+                if ($update->getChosenInlineResult()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_CALLBACK_QUERY)) {
+                if ($update->getCallbackQuery()) {
+                    $action->run($request, $update);
+                }
+            } elseif ($action = $this->getAction(self::EVENT_PRE_CHECKOUT_QUERY)) {
+                if ($update->getPreCheckoutQuery()) {
+                    $action->run($request, $update);
+                }
             }
         }
     }
